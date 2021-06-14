06f91fb70885c4d91c17371a9bc86e833d504cd4
HanSeo0507 <khan3618@naver.com>
2021-06-15 05:11:05 +0900

    Updates

---
+++ precache-manifest.7c193995db296120f89207599e6e88a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a2d40b36e6f73ec17a1b36e6cbf68e",
+    "url": "/What-Is-My-IP/index.html"
+  },
+  {
+    "revision": "c1653fe1ab6e5297414e",
+    "url": "/What-Is-My-IP/static/css/main.edd63e37.chunk.css"
+  },
+  {
+    "revision": "39db58c3c8dfc690aa0d",
+    "url": "/What-Is-My-IP/static/js/2.25da4dca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/What-Is-My-IP/static/js/2.25da4dca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1653fe1ab6e5297414e",
+    "url": "/What-Is-My-IP/static/js/main.de6e84e5.chunk.js"
+  },
+  {
+    "revision": "c0ab4fc670969833bf17",
+    "url": "/What-Is-My-IP/static/js/runtime-main.7b3e6042.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0101df7d85f4f09895a3f37e979a4944.js"
+  "/What-Is-My-IP/precache-manifest.7c193995db296120f89207599e6e88a4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/What-Is-My-IP/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
